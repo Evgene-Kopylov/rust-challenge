7741f66ee562db5657700fc143a698353e613a71
Evgene-Kopylov <evgenekopylov@gmail.com>
2025-05-24 10:48:38 +0200

    fix

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use pipeline::calculate_user_stats;
 
 fn main() -> anyhow::Result<()> {
     let transfers = DefaultTransferGenerator::default().generate(10_000)?;
-    let stats = calculate_user_stats(&transfers);
+    let stats = calculate_user_stats(&transfers)?;
 
     for stat in stats.iter().take(10) {
         println!("{:#?}", stat);
